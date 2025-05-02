33bbec5bfbbc71f838cf30852295e33e708c412c
Орел Михаил <77534887+m0khm@users.noreply.github.com>
2025-05-02 10:59:38 +0300

    Update telegram_notifier.py

--- src/telegram_notifier.py
+++ src/telegram_notifier.py
@@ -1,9 +1,12 @@
 from telegram import Bot
+import os
 
 class TelegramNotifier:
-    def __init__(self, bot_token: str, chat_id: str):
+    def __init__(self, bot_token: str, chat_ids: list[str]):
         self.bot = Bot(token=bot_token)
-        self.chat_id = chat_id
+        # Приводим все ID к строкам (или int, на ваш вкус)
+        self.chat_ids = [cid.strip() for cid in chat_ids]
 
-    def notify(self, text: str):
-        self.bot.send_message(chat_id=self.chat_id, text=text)
+    def notify(self, text: str, parse_mode: str = None):
+        for chat_id in self.chat_ids:
+            self.bot.send_message(chat_id=chat_id, text=text, parse_mode=parse_mode)
